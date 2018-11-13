f1075149ef9169a95857368ff98362a17db813a0
Heisenberg017 <suyash9211@gmail.com>
2018-11-13 17:30:16 +0530

    Work 13/11/2018

---
+++ School_Dashboard/script.js
@@ -0,0 +1,33 @@
+$("#filter-sort").on("click", function(e) {
+  e.preventDefault();
+
+  if ($(this).hasClass("open")) {
+    $(this).removeClass("open");
+    $(".filter").slideUp("fast");
+  } else {
+    $(this).addClass("open");
+    $(".filter").slideDown("fast");
+  }
+});
+$(function() {
+  transition_timeout = 40;
+
+  $(".title_items").click(function() {
+    current = $(this)
+      .next()
+      .find("li");
+
+    $(this).toggleClass("active");
+    current.toggleClass("visible");
+
+    if ($(this).hasClass("active")) {
+      for (i = 0; i <= current.length; i++) {
+        $(current[i]).css("transition-delay", transition_timeout * i + "ms");
+      }
+    } else {
+      for (i = current.length, j = -1; i >= 0; i--, j++) {
+        $(current[i]).css("transition-delay", transition_timeout * j + "ms");
+      }
+    }
+  });
+});
