1976e242ad0e0b994ab63e1063eeedceb23c5098
Heisenberg017 <suyash9211@gmail.com>
2019-01-17 17:09:22 +0530

    Work 17/01/2019

--- School_Dashboard_New/assets/js/script.js
+++ School_Dashboard_New/assets/js/script.js
@@ -68,10 +68,3 @@ jQuery(document).ready(function ($) {
     $(tab_content).addClass("active");
   });
 });
-
-$(window).on("load resize ", function () {
-  var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-  $('.tbl-header').css({
-    'padding-right': scrollWidth
-  });
-}).resize();
